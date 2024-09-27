5aef1e084db68b3151df23a4e0f538aa74f4f94b
TU-hayaoki <156896546+TU-hayaoki@users.noreply.github.com>
2024-09-27 16:03:21 +0900

    Initial Commit

---
+++ SwiftDataSample/SwiftDataSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataSampleApp.swift
+//  SwiftDataSample
+//
+//  Created by Hayato Aoki on 2024/09/27.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
